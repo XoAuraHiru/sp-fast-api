ece3ac30f698fa4ca87c45f7912faf371d6c46ff
Hirun Chamara <astronomyhirunchamara@gmail.com>
2025-02-05 11:21:30 +0530

    fixed the issue where string needed to be length specified

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import models
+from config.db import engine
 
-app = FastAPI()
-
+app = FastAPI(title="Todo App API")
+models.Base.metadata.create_all(bind=engine)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Todos(Base):
     __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(100))
+    description = Column(String(1000))
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
