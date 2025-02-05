2177e12231815cf9dbe3d5b49521118160c051ec
Hirun Chamara <astronomyhirunchamara@gmail.com>
2025-02-05 12:36:49 +0530

    added todo model in models directry for the mvc

---
+++ models/todo.py
@@ -0,0 +1,14 @@
+from config.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, func, DateTime
+
+
+class Todos(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String(1000))
+    priority = Column(Integer)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
